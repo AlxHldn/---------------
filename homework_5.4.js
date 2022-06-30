39e5563f07977e15dc98aa4bba7e48b18f20e931
Lapa1215 <lopatin.it@mail.ru>
2022-06-30 22:04:45 +0700

    homework_5.4_Done

---
+++ homework_5.4.js
@@ -0,0 +1,38 @@
+// Задание 1 вариант 1
+
+let inputData = prompt("Введите значение, которое хотите отзеркалить");
+
+function reversData(string) {
+  let newRewersString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    newRewersString += string[i];
+  }
+  return newRewersString;
+}
+
+console.log(reversData(inputData));
+
+// Задание 1 вариант 2
+
+let inputData1 = prompt("Введите значение, которое хотите отзеркалить");
+
+function reversData1(string) {
+  let arrayData = [...string];
+  let reversArray = arrayData.reverse();
+  let newRewersString = reversArray.join("");
+  return newRewersString;
+}
+
+console.log(reversData1(inputData1));
+
+// Задание 2
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let number = random(0, 101);
+
+console.log(number);
